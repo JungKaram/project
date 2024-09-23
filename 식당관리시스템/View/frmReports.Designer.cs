de13b86bb46ef61e2d9771e7ec17c54795a7e43d
Karam <kkk_0522@naver.com>
2024-09-23 16:09:38 +0900

    Add files via upload

---
+++ 식당관리시스템/View/frmReports.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace RM
+{
+    partial class frmReports
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnMenu = new Guna.UI2.WinForms.Guna2Button();
+            this.btnStaff = new Guna.UI2.WinForms.Guna2Button();
+            this.btnSaleCat = new Guna.UI2.WinForms.Guna2Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(45, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Reports";
+            // 
+            // btnMenu
+            // 
+            this.btnMenu.BackColor = System.Drawing.Color.White;
+            this.btnMenu.CustomizableEdges.BottomRight = false;
+            this.btnMenu.CustomizableEdges.TopRight = false;
+            this.btnMenu.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnMenu.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnMenu.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnMenu.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnMenu.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(55)))), ((int)(((byte)(89)))));
+            this.btnMenu.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnMenu.ForeColor = System.Drawing.Color.White;
+            this.btnMenu.Image = global::RM.Properties.Resources.clip;
+            this.btnMenu.ImageAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.btnMenu.ImageOffset = new System.Drawing.Point(10, 0);
+            this.btnMenu.Location = new System.Drawing.Point(51, 184);
+            this.btnMenu.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnMenu.Name = "btnMenu";
+            this.btnMenu.Size = new System.Drawing.Size(180, 94);
+            this.btnMenu.TabIndex = 1;
+            this.btnMenu.Text = "Menu List";
+            this.btnMenu.Click += new System.EventHandler(this.btnMenu_Click);
+            // 
+            // btnStaff
+            // 
+            this.btnStaff.CustomizableEdges.BottomRight = false;
+            this.btnStaff.CustomizableEdges.TopRight = false;
+            this.btnStaff.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnStaff.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnStaff.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnStaff.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnStaff.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(55)))), ((int)(((byte)(89)))));
+            this.btnStaff.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnStaff.ForeColor = System.Drawing.Color.White;
+            this.btnStaff.Image = global::RM.Properties.Resources.clip;
+            this.btnStaff.ImageAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.btnStaff.ImageOffset = new System.Drawing.Point(10, 0);
+            this.btnStaff.Location = new System.Drawing.Point(237, 184);
+            this.btnStaff.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnStaff.Name = "btnStaff";
+            this.btnStaff.Size = new System.Drawing.Size(180, 94);
+            this.btnStaff.TabIndex = 2;
+            this.btnStaff.Text = "Staff List";
+            this.btnStaff.Click += new System.EventHandler(this.btnStaff_Click);
+            // 
+            // btnSaleCat
+            // 
+            this.btnSaleCat.CustomizableEdges.BottomRight = false;
+            this.btnSaleCat.CustomizableEdges.TopRight = false;
+            this.btnSaleCat.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnSaleCat.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnSaleCat.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnSaleCat.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnSaleCat.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(55)))), ((int)(((byte)(89)))));
+            this.btnSaleCat.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnSaleCat.ForeColor = System.Drawing.Color.White;
+            this.btnSaleCat.Image = global::RM.Properties.Resources.clip;
+            this.btnSaleCat.ImageAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.btnSaleCat.ImageOffset = new System.Drawing.Point(10, 0);
+            this.btnSaleCat.Location = new System.Drawing.Point(423, 184);
+            this.btnSaleCat.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnSaleCat.Name = "btnSaleCat";
+            this.btnSaleCat.Size = new System.Drawing.Size(180, 94);
+            this.btnSaleCat.TabIndex = 3;
+            this.btnSaleCat.Text = "Sale By Category";
+            this.btnSaleCat.Click += new System.EventHandler(this.btnSaleCat_Click);
+            // 
+            // frmReports
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 529);
+            this.Controls.Add(this.btnSaleCat);
+            this.Controls.Add(this.btnStaff);
+            this.Controls.Add(this.btnMenu);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "frmReports";
+            this.Text = "frmReports";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private Guna.UI2.WinForms.Guna2Button btnMenu;
+        private Guna.UI2.WinForms.Guna2Button btnStaff;
+        private Guna.UI2.WinForms.Guna2Button btnSaleCat;
+    }
+}
